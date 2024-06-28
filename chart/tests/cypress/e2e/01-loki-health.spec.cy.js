1783b16df8c40383772b422d1b0b89a689fccf62
Jimmy Ungerman <jimmy@defenseunicorns.com>
2024-06-28 18:39:21 +0000

    Loki logging cypress

--- chart/tests/cypress/e2e/01-loki-health.spec.cy.js
+++ chart/tests/cypress/e2e/01-loki-health.spec.cy.js
@@ -2,7 +2,7 @@
     {
       retries:
       {
-        runMode: 180,
+        runMode: 2,
       }
     }, () => {
     Cypress.on('uncaught:exception', (err, runnable) => {
@@ -11,8 +11,8 @@
   
     if (Cypress.env("check_datasource")) {
   
-        it('Check Loki is available as a data source in grafana and connected', function() {
-            cy.wait(1000); 
+        it('Check Loki is available as a data source in grafana and connected', {retries: 2}, function() {
+            cy.wait(10000); 
             const saveOutputOptions = ['Data source is working', 'Data source successfully connected']
             const saveOutput = new RegExp(`${saveOutputOptions.join('|')}`, 'g')
 
@@ -29,7 +29,7 @@
             // Click on the 'Save & test` button
             cy.get('button[type="submit"]').click()
             // Check to ensure the data source is working
-            cy.get('.p-t-2').contains(saveOutput, { timeout: 30000 });
+            cy.get('.p-t-2').contains(saveOutput, { timeout: 120000 });
           })
 
         it('Test for Loki Dashboard log data', function () {
@@ -37,6 +37,8 @@
             cy.wait(1000);
             cy.loadGrafanaDashboard("Loki Dashboard quick search");
             cy.wait(1000);
+            cy.get('[id=var-namespace]').click()
+            cy.get('[data-testid*="istio-system"]').click()
             cy.get('[class$=-logs-row]').should('have.length.at.least', 10);
           });
     }
