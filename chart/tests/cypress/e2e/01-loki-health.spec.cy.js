a941f2bf8c640b382c09d9d01c71b54ff2ccc352
Kirby Liu <kliu@vivsoft.io>
2024-10-07 20:37:27 +0000

    Increase timeout, retry for cypress testing and add skip logic for grafanalogin

--- chart/tests/cypress/e2e/01-loki-health.spec.cy.js
+++ chart/tests/cypress/e2e/01-loki-health.spec.cy.js
@@ -11,13 +11,22 @@
   
     if (Cypress.env("check_datasource")) {
   
-        it('Check Loki is available as a data source in grafana and connected', {retries: 2}, function() {
+        it('Check Loki is available as a data source in grafana and connected', {retries: 7}, function() {
             cy.wait(10000); 
             const saveOutputOptions = ['Data source is working', 'Data source successfully connected', 'Successfully connected to']
             const saveOutput = new RegExp(`${saveOutputOptions.join('|')}`, 'g')
 
             cy.visit(Cypress.env('grafana_url'))
-            cy.performGrafanaLogin('admin', 'prom-operator')
+        .then(() => {
+          // Check if the URL contains '/login'
+          cy.url().then(currentUrl => {
+            if (currentUrl.includes('/login')) {
+              // Perform login if the URL contains '/login'
+              cy.performGrafanaLogin('admin', 'prom-operator');
+            }
+          });
+        });
+
             // Visit the datasources page
             cy.visit(`${Cypress.env('grafana_url')}/connections/datasources`)
   
@@ -29,7 +38,7 @@
             // Click on the 'Save & test` button
             cy.get('button[type="submit"]').click()
             // Check to ensure the data source is working
-            cy.get('[data-testid="data-testid Alert success"]').contains(saveOutput, { timeout: 180000 })
+            cy.get('[data-testid="data-testid Alert success"]', { timeout: 30000 }).contains(saveOutput)
           })
 
         it('Test for Loki Dashboard log data', function () {
